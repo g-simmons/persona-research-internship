4cc42e482019283b4794fc20c0ef1f8fa064368d
Brody Roberts <72287521+brody2002@users.noreply.github.com>
2023-10-30 00:36:52 -0700

    embeddiny.py now has schemas using pandera
    This file now checks the schemas of the input and ouput Jsonl files.

--- cloud_embedding/embedding.py
+++ cloud_embedding/embedding.py
@@ -1,11 +1,52 @@
 import cohere
 import pandas 
+import pandera
 
 
 # Cloud based embedding
 
 # Cohere Key:
 co = cohere.Client("51F5llrM1i81Mp4A2HJ0TpApR4FF9Lpn12Nc90pN")
+
+# Sets up the wanted schema for the input "test.json"
+# Only checks for strings inside the list
+expected_input_schema = pandera.DataFrameSchema(
+   {
+    "Prompts": pandera.Column(str)
+   }
+)
+# Sets up the wanted schema for the output "output.jsonl"
+
+expected_output_schema = pandera.DataFrameSchema(
+   {
+    "prompt": pandera.Column(str),
+    "prompt_embedding": pandera.Column(list[float])
+   }
+)
+
+# raises error flag if schema doesn't match 
+def error_flag(dict,schema,str):
+  try:
+    schema.validate(dict, lazy=True)
+  except pandera.errors.SchemaErrors as err:
+    print(str)
+    print(err)
+
+
+#For some reason, we can't run a checker on the list[float] above... so we need this function
+def embedding_checker(embeddings):
+
+  for i in range(0, len(embeddings)):
+    test = pandas.DataFrame({
+      "prompt_embedding" : embeddings[i]
+    })
+
+    test_schema = pandera.DataFrameSchema(
+      {
+        "prompt_embedding": pandera.Column(float, pandera.Check.less_than_or_equal_to(10),pandera.Check.greater_than_or_equal_to(-10))
+      }
+    )
+    error_flag(test, test_schema, f"PROMPT EMBEDDING INFORMATION ERROR {i}")
 
 
 # Find Json from input path
@@ -16,7 +57,8 @@ def json_input_directory() -> dict:
     # puts Json Data in varaible
     dictionary = pandas.read_json(raw_dir_path)
 
-    
+    #Implement Panderas checking the "dictionary" var in this function
+
     return dictionary
    
 
@@ -33,17 +75,24 @@ def json_to_list(dict) -> list:
 
 
 # Creates json for output
-def json_output(embedding_list, prompt_list):
+def json_output(embedding_list, prompt_list) -> dict:
   #sets up the dictionary frame
-  df = pandas.DataFrame({'prompt': prompt_list,'prompt-embedding': embedding_list})
 
+  df = pandas.DataFrame({'prompt': prompt_list,'prompt_embedding': embedding_list})
+  print(df)
   # Save the DataFrame to a JSONL file
   df.to_json('output.jsonl', orient='records', lines=True)
+  
+  return df
 
 
 # Main ----------------------------------------------------------------------------------------
 
+
 dict = json_input_directory()
+
+error_flag(dict, expected_input_schema, "INPUT SCHEMA ERROR")
+
 list = json_to_list(dict)
 
 # Cohere Cloud Embedding
@@ -52,5 +101,18 @@ response = co.embed(
   model='small', 
 )
 
-json_output(response.embeddings, list)
+output_dict = json_output(response.embeddings, list)
+
+
+error_flag(output_dict, expected_output_schema, "OUTPUT SCHEMA ERROR")
+
+
+
+
+
+
+
+
+
+
 
