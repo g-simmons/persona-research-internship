be5ed0357af31983b3bca4921b12201a6ff77188
ria-bhandari <riabhan@gmail.com>
2024-02-03 14:38:56 -0800

    updated for maple

--- tda_carlsson's_method/convert_jsonl_to_csv.py
+++ tda_carlsson's_method/convert_jsonl_to_csv.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 import os 
 
-jsonl_file_path = "/Users/riab/Documents/persona-research-internship/data/llm_edited_reviews/llm_edited_reviews.jsonl"
+jsonl_file_path = "/Users/riab/Documents/persona-research-internship/data/dim_reduced_movie_reviews/all_data_2.jsonl"
 
 file_data = [] # initialize list to store data 
 
@@ -13,12 +13,12 @@ with open(jsonl_file_path, 'r') as f:
         file_data.append(json_file_data)
 
 # create a pandas DataFrame from the list 
-df = pd.DataFrame(file_data)["target_sentiment_level"]
+df = pd.DataFrame(file_data)["full_prompt_embedding"]
 
 
 directory_to_save_file = "/Users/riab/Documents/persona-research-internship/data/csv_converted_data"
 os.makedirs(directory_to_save_file, exist_ok=True)
 
 # save the DataFrame to a csv file in directory created above 
-csv_file_path = os.path.join(directory_to_save_file, 'llm_edited_reviews.csv')
-df.to_csv(csv_file_path, index=False, header=["target_sentiment_level"])
+csv_file_path = os.path.join(directory_to_save_file, 'all_data_2.csv')
+df.to_csv(csv_file_path, index=False, header=["full_prompt_embedding"])
