fc50ad898760bd9100c22e45a469488a540b9c09
ria-bhandari <riabhan@gmail.com>
2024-02-03 14:10:47 -0800

    added converted csv files

--- tda_carlsson's_method/convert_jsonl_to_csv.py
+++ tda_carlsson's_method/convert_jsonl_to_csv.py
@@ -1,8 +1,23 @@
-# In this file, the dvc data files that are in jsonl format are converted to csv
-
 import pandas as pd
+import json
+import os 
 
-file_path = '/Users/riab/persona-research-internship/data/llm_edited_reviews/llm_edited_reviews.jsonl'
-df = pd.read_json(file_path, lines = True)
-embeddings = df["embeddings"]
-pd.DataFrame(embeddings).to_csv('llm_edited_reviews.csv')
+jsonl_file_path = "/Users/riab/Documents/persona-research-internship/data/dim_reduced_movie_reviews/all_data_2.jsonl"
+
+file_data = [] # initialize list to store data 
+
+# read the jsonl file and append to the list
+with open(jsonl_file_path, 'r') as f:
+    for line in f:
+        json_file_data = json.loads(line)
+        file_data.append(json_file_data)
+
+# create a pandas DataFrame from the list 
+df = pd.DataFrame(file_data)
+
+directory_to_save_file = "/Users/riab/Documents/persona-research-internship/data/csv_converted_data"
+os.makedirs(directory_to_save_file, exist_ok=True)
+
+# save the DataFrame to a csv file in directory created above 
+csv_file_path = os.path.join(directory_to_save_file, 'all_data_2.csv')
+df.to_csv(csv_file_path, index=False)
