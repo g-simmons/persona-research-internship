6fb7d97f098a778d2b5e1ea90e17eeeeb97ad390
ria-bhandari <riabhan@gmail.com>
2024-02-24 15:42:36 -0800

    updated the data for sentiment

--- tda_carlsson's_method/convert_with_sentiment.py
+++ tda_carlsson's_method/convert_with_sentiment.py
@@ -1,10 +1,10 @@
 # import pandas as pd
 # import json
-# import os 
+# import os
 
 # jsonl_file_path = "/Users/riab/Documents/persona-research-internship/data/llm_edited_reviews_embedded/llm_edited_reviews_embedded.jsonl"
 
-# file_data = [] # initialize list to store data 
+# file_data = [] # initialize list to store data
 
 # # read the jsonl file and append to the list
 # with open(jsonl_file_path, 'r') as f:
@@ -12,7 +12,7 @@
 #         json_file_data = json.loads(line)
 #         file_data.append(json_file_data)
 
-# # create a pandas DataFrame from the list 
+# # create a pandas DataFrame from the list
 # df = pd.DataFrame(file_data)["full_prompt_embedding"]
 # print(type(df["full_prompt_embedding"].values[0]))
 
@@ -21,33 +21,38 @@
 # os.makedirs(directory_to_save_file, exist_ok=True)
 
 
-
-# # save the DataFrame to a csv file in directory created above 
+# # save the DataFrame to a csv file in directory created above
 # csv_file_path = os.path.join(directory_to_save_file, 'llm_edited_reviews_embedded.csv')
 # df.to_csv(csv_file_path, index=False, header=["full_prompt_embedding"])
 
 import pandas as pd
 import json
-import os 
+import os
 import numpy as np
 
 jsonl_file_path = "/Users/riab/Documents/persona-research-internship/data/llm_edited_reviews_embedded_with_sentiment/llm_edited_reviews_embedded_with_sentiment.jsonl"
 
-file_data = [] # initialize list to store data 
+file_data = []  # initialize list to store data
 
 # read the jsonl file and append to the list
-with open(jsonl_file_path, 'r') as f:
+with open(jsonl_file_path, "r") as f:
     for line in f:
         json_file_data = json.loads(line)
         file_data.append(json_file_data)
 
-# create a pandas DataFrame from the list 
+# create a pandas DataFrame from the list
 orig = pd.DataFrame(file_data)
 
-# # ["full_prompt_embedding"]
-df = pd.DataFrame(orig["full_prompt_embedding"].tolist())
-#df["sentiment"] = 1 # TODO compute actual sentiment scores
+extend_embeddings = []
 
-df.to_csv("../data/llm_edited_reviews_embedded_with_sentiment/llm_edited_reviews_embedded_with_sentiment.csv", index=False)
+for i, j in orig.iterrows():
+    embedding = j["full_prompt_embedding"].copy()
+    embedding.append(j["full_prompt_sentiment"])
+    extend_embeddings.append(embedding)
 
-#print(df["sentiment"])
+df_final = pd.DataFrame(extend_embeddings)
+
+df_final.to_csv(
+    "../data/llm_edited_reviews_embedded_with_sentiment/llm_edited_reviews_embedded_with_sentiment.csv",
+    index=False,
+)
