5f7fb9b97abc0c488a5fd8b1c810033344f60f0e
Raymond Ran <ranr9131@gmail.com>
2024-07-09 18:48:05 -0700

    revert

---
+++ get_embeddings.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import openai
+import jsonlines
+
+embedding_model = "text-embedding-3-small"
+embedding_encoding = "cl100k_base"
+max_tokens = 8000
+
+
+# loading hackaprompt dataset
+df = pd.read_parquet("hf://datasets/hackaprompt/hackaprompt-dataset/hackaprompt.parquet")
+df = df[["user_input", "token_count"]]
+df = df.dropna()
+
+
+# subsample to 1k most recent reviews and remove samples that are too long
+top_n = 3
+df = df.head(top_n * 2)  # first cut to first 2k entries, assuming less than half will be filtered out
+
+
+# omit reviews that are too long to embed
+df = df[df.token_count <= max_tokens].head(top_n)
+
+
+
+client = openai.OpenAI(api_key="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")
+
+def get_embedding(text, model = embedding_model):
+    text = text.replace("\n", " ")
+    return client.embeddings.create(input = [text], model=model).data[0].embedding
+
+df["embedding"] = df.user_input.apply(lambda x: get_embedding(x, model=embedding_model))
+# df.to_csv("hackaprompts.csv")
+
+
+
+df.to_json(lines=True, orient="records", path_or_buf="embeddings.jsonl")
