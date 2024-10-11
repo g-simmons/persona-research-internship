083c93bff0b8c3bcc3c665323e05e9e0ab3d7578
Raymond Ran <ranr9131@gmail.com>
2024-10-11 12:12:31 -0700

    unpacking freq

--- LLM_Categorical_Hierarchical_Representations/test.py
+++ LLM_Categorical_Hierarchical_Representations/test.py
@@ -1,23 +1,14 @@
 #!/usr/bin/env python3
 
-import ontology_class
-import os
 
-test = ontology_class.Onto("owl/ado.owl")
+import json
+import ast
 
-all_noun_synsets = test.all_synsets()
 
-print([i.name() for i in all_noun_synsets])
-print(len(all_noun_synsets))
+term_freq = {}
+with open('term__frequencies/wordnet-frequencies.json', 'r') as f:
+    term_freq = json.load(f)
 
-for filename in os.listdir("owl"):
-    try:
-        test = ontology_class.Onto(f"owl/{filename}")
-        all_noun_synsets = test.all_synsets()
-        if len(all_noun_synsets) > 1000:
-            with open(f"textfiles/{filename.split(".")[0]}.txt", "w") as f:
-                for i in all_noun_synsets:
-                    if i.name() != None:
-                        f.write(i.name() + "\n")
-    except Exception as e:
-        print(Exception)
+wordnet_freq = term_freq["wordnet.txt"]
+
+print(wordnet_freq["entity"])
