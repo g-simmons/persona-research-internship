bf93e1342f8c33abf0bde3fcb5369fbdf4596299
Gabriel Simmons <simmons.gabe@gmail.com>
2025-02-16 13:19:18 -0800

    formatting

--- llm_ontology/llm_ontology/obo_ontology_heatmaps.py
+++ llm_ontology/llm_ontology/obo_ontology_heatmaps.py
@@ -5,6 +5,7 @@
 
 import json
 import networkx as nx
+
 # from nltk.corpus import wordnet as wn
 from transformers import AutoTokenizer, PreTrainedTokenizer, PreTrainedTokenizerFast
 import pathlib
@@ -20,7 +21,8 @@ from typing import List, Tuple
 import matplotlib.pyplot as plt
 import hierarchical as hrc
 import warnings
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 
 import ontology_class
 
@@ -28,14 +30,18 @@ import ontology_class
 logger = logging.getLogger(__name__)
 if not logger.handlers:
     # Create file handler
-    file_handler = logging.FileHandler('obo_ontology_heatmaps.log')
-    file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler = logging.FileHandler("obo_ontology_heatmaps.log")
+    file_formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
     file_handler.setFormatter(file_formatter)
     logger.addHandler(file_handler)
 
     # Create stdout handler
     stdout_handler = logging.StreamHandler()
-    stdout_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    stdout_formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
     stdout_handler.setFormatter(stdout_formatter)
     logger.addHandler(stdout_handler)
 
@@ -57,9 +63,12 @@ def merge_nodes(G: nx.DiGraph, lemma_dict: dict):
         if len(children) == 1:
             child = children[0]
             parent_lemmas_not_in_child = lemma_dict[node] - lemma_dict[child]
-            if len(list(G.predecessors(child))) == 1 or len(parent_lemmas_not_in_child) < 6:
+            if (
+                len(list(G.predecessors(child))) == 1
+                or len(parent_lemmas_not_in_child) < 6
+            ):
                 grandchildren = list(G.successors(child))
-                
+
                 # if len(parent_lemmas_not_in_child) > 1:
                 #     if len(grandchildren) > 0:
                 #         lemma_dict[node + '.other'] = parent_lemmas_not_in_child
@@ -73,7 +82,9 @@ def merge_nodes(G: nx.DiGraph, lemma_dict: dict):
                 logger.info(f"merged {node} and {child}")
 
 
-def _noun_to_gemma_vocab_elements(word, multi: bool, vocab_set: set[str], p: inflect.engine) -> list[str]:
+def _noun_to_gemma_vocab_elements(
+    word, multi: bool, vocab_set: set[str], p: inflect.engine
+) -> list[str]:
     word = word.lower()
     plural = p.plural(word)
     if multi:
@@ -97,17 +108,22 @@ def _noun_to_gemma_vocab_elements(word, multi: bool, vocab_set: set[str], p: inf
         add_space = [w for w in add_cap_and_plural]
         return vocab_set.intersection(add_space)
 
+
 def save_ontology_hypernym(params: str, step: str, ontology_name: str, multi: bool):
 
     ontology_dir = BIGSTORAGE_DIR / "raymond" / "owl" / f"{ontology_name}.owl"
     # model_name = "gemma-2b"
     # tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b")
     model_name = "pythia"
-    
+
     tokenizer = AutoTokenizer.from_pretrained(
         f"EleutherAI/pythia-{params}-deduped",
         revision=f"{step}",
-        cache_dir=BIGSTORAGE_DIR / "raymond" / "huggingface_cache" / f"pythia-{params}-deduped" / step,
+        cache_dir=BIGSTORAGE_DIR
+        / "raymond"
+        / "huggingface_cache"
+        / f"pythia-{params}-deduped"
+        / step,
     )
 
     vocab = tokenizer.get_vocab()
@@ -121,10 +137,11 @@ def save_ontology_hypernym(params: str, step: str, ontology_name: str, multi: bo
         lemmas = set()
         for hyponym in hyponyms:
             lemmas.update(lemma.name() for lemma in hyponym.lemmas())
-            lemmas.update(get_all_hyponym_lemmas(hyponym))  # Recursively get lemmas from hyponyms,
-        
-        return lemmas
+            lemmas.update(
+                get_all_hyponym_lemmas(hyponym)
+            )  # Recursively get lemmas from hyponyms,
 
+        return lemmas
 
     # all_noun_synsets = list(wn.all_synsets(pos=wn.NOUN))
     test = ontology_class.Onto(ontology_dir)
@@ -133,7 +150,7 @@ def save_ontology_hypernym(params: str, step: str, ontology_name: str, multi: bo
     noun_lemmas = {}
     for s in all_noun_synsets:
         lemmas = get_all_hyponym_lemmas(s)
-        if model_name == "gemma-2b": # add and remove space bc of how gemma vocab works
+        if model_name == "gemma-2b":  # add and remove space bc of how gemma vocab works
             lemmas = vocab_set.intersection({"▁" + l for l in lemmas})
             noun_lemmas[s.name()] = {l[1:] for l in lemmas}
         elif model_name == "pythia":
@@ -144,13 +161,15 @@ def save_ontology_hypernym(params: str, step: str, ontology_name: str, multi: bo
                 for i in range(len(lemmas)):
                     if lemmas_split[i].issubset(vocab_set):
                         lemmas_included.append(lemmas[i])
-                
 
                 noun_lemmas[s.name()] = set(lemmas_included)
             else:
                 lemmas = vocab_set.intersection(lemmas)
                 noun_lemmas[s.name()] = lemmas
             
+            
+            
+
             
     large_nouns = {k: v for k, v in noun_lemmas.items() if len(v) > 1}
 
@@ -165,11 +184,9 @@ def save_ontology_hypernym(params: str, step: str, ontology_name: str, multi: bo
             ancestors = [s.name() for s in path if s.name() in nodes]
 
             if len(ancestors) > 1:
-                G_noun.add_edge(ancestors[-2],key) # first entry is itself
+                G_noun.add_edge(ancestors[-2], key)  # first entry is itself
             else:
                 logger.info(f"no ancestors for {key}")
-
-
 
     merge_nodes(G_noun, large_nouns)
     # logger.info("SECOND MERGE")
@@ -177,143 +194,41 @@ def save_ontology_hypernym(params: str, step: str, ontology_name: str, multi: bo
 
     large_nouns = {k: v for k, v in large_nouns.items() if k in G_noun.nodes()}
 
-
     G_noun = nx.DiGraph(G_noun.subgraph(nodes))
 
-
-
-
-    # make a gemma specific version
-    def _noun_to_gemma_vocab_elements(word):
-        word = word.lower()
-        plural = p.plural(word)
-        if multi:
-            word_list = word.split("_")
-            plural_list = plural.split("_")
-            word_cap_list = word.capitalize().split("_")
-            plural_cap_list = plural.capitalize().split("_")
-
-            add_cap_and_plural = [word_list, plural_list, word_cap_list, plural_cap_list]
-            corr_words = [word, plural, word.capitalize(), plural.capitalize()]
-
-            included = []
-            for i in range(len(add_cap_and_plural)):
-                if set(add_cap_and_plural[i]).issubset(vocab_set):
-                    included.append(corr_words[i])
-
-            return included
-        else:
-            add_cap_and_plural = [word, word.capitalize(), plural, plural.capitalize()]
-            # add_space = ["▁" + w for w in add_cap_and_plural]
-            add_space = [w for w in add_cap_and_plural]
-            return vocab_set.intersection(add_space)
-
-    
-
-    # make a gemma specific version
-    def _noun_to_gemma_vocab_elements(word):
-        word = word.lower()
-        plural = p.plural(word)
-        if multi:
-            word_list = word.split("_")
-            plural_list = plural.split("_")
-            word_cap_list = word.capitalize().split("_")
-            plural_cap_list = plural.capitalize().split("_")
-
-            add_cap_and_plural = [word_list, plural_list, word_cap_list, plural_cap_list]
-            corr_words = [word, plural, word.capitalize(), plural.capitalize()]
-
-            included = []
-            for i in range(len(add_cap_and_plural)):
-                if set(add_cap_and_plural[i]).issubset(vocab_set):
-                    included.append(corr_words[i])
-
-            return included
-        else:
-            add_cap_and_plural = [word, word.capitalize(), plural, plural.capitalize()]
-            # add_space = ["▁" + w for w in add_cap_and_plural]
-            add_space = [w for w in add_cap_and_plural]
-            return vocab_set.intersection(add_space)
-
-    
-
-
-    # make a gemma specific version
-    def _noun_to_gemma_vocab_elements(word):
-        word = word.lower()
-        plural = p.plural(word)
-        if multi:
-            word_list = word.split("_")
-            plural_list = plural.split("_")
-            word_cap_list = word.capitalize().split("_")
-            plural_cap_list = plural.capitalize().split("_")
-
-            add_cap_and_plural = [word_list, plural_list, word_cap_list, plural_cap_list]
-            corr_words = [word, plural, word.capitalize(), plural.capitalize()]
-
-            included = []
-            for i in range(len(add_cap_and_plural)):
-                if set(add_cap_and_plural[i]).issubset(vocab_set):
-                    included.append(corr_words[i])
-
-            return included
-        else:
-            add_cap_and_plural = [word, word.capitalize(), plural, plural.capitalize()]
-            # add_space = ["▁" + w for w in add_cap_and_plural]
-            add_space = [w for w in add_cap_and_plural]
-            return vocab_set.intersection(add_space)
-
-    
-
-    # make a gemma specific version
-    def _noun_to_gemma_vocab_elements(word):
-        word = word.lower()
-        plural = p.plural(word)
-        if multi:
-            word_list = word.split("_")
-            plural_list = plural.split("_")
-            word_cap_list = word.capitalize().split("_")
-            plural_cap_list = plural.capitalize().split("_")
-
-            add_cap_and_plural = [word_list, plural_list, word_cap_list, plural_cap_list]
-            corr_words = [word, plural, word.capitalize(), plural.capitalize()]
-
-            included = []
-            for i in range(len(add_cap_and_plural)):
-                if set(add_cap_and_plural[i]).issubset(vocab_set):
-                    included.append(corr_words[i])
-
-            return included
-        else:
-            add_cap_and_plural = [word, word.capitalize(), plural, plural.capitalize()]
-            # add_space = ["▁" + w for w in add_cap_and_plural]
-            add_space = [w for w in add_cap_and_plural]
-            return vocab_set.intersection(add_space)
-
     ## save the data
-    ontology_data_path = DATA_DIR / "ontologies" / f"noun_synsets_ontology_pythia_{ontology_name}.json"
-    with open(ontology_data_path, 'w') as f:
+    ontology_data_path = (
+        DATA_DIR / "ontologies" / f"noun_synsets_ontology_pythia_{ontology_name}.json"
+    )
+    with open(ontology_data_path, "w") as f:
         prev_pythia_words = []
         corr_synsets = []
         for synset, lemmas in large_nouns.items():
             pythia_words = []
             for w in lemmas:
-                pythia_words.extend(_noun_to_gemma_vocab_elements(w, multi, vocab_set, p))
+                pythia_words.extend(
+                    _noun_to_gemma_vocab_elements(w, multi, vocab_set, p)
+                )
                 if synset == "organism_substance":
                     logger.info(w)
 
             if pythia_words in prev_pythia_words:
-                logger.info(f"repeated: {synset}")        # for repeating synset lemmas
+                logger.info(f"repeated: {synset}")  # for repeating synset lemmas
             prev_pythia_words.append(pythia_words)
             corr_synsets.append(synset)
 
             f.write(json.dumps({synset: pythia_words}) + "\n")
-            
-    graph_path = DATA_DIR / "ontologies" / f"noun_synsets_ontology_hypernym_graph_{ontology_name}.adjlist"
+
+    graph_path = (
+        DATA_DIR
+        / "ontologies"
+        / f"noun_synsets_ontology_hypernym_graph_{ontology_name}.adjlist"
+    )
     nx.write_adjlist(G_noun, graph_path)
 
+
 def update_vocab_dict(tokenizer: PreTrainedTokenizer | PreTrainedTokenizerFast) -> dict:
-    vocab_dict = tokenizer.get_vocab() # type: ignore
+    vocab_dict = tokenizer.get_vocab()  # type: ignore
     new_vocab_dict = {}
     for key, value in vocab_dict.items():
         new_key = key.replace(" ", "_")
@@ -321,7 +236,10 @@ def update_vocab_dict(tokenizer: PreTrainedTokenizer | PreTrainedTokenizerFast) 
     vocab_dict = new_vocab_dict
     return vocab_dict
 
-def get_mats(params: str, step: str, multi: bool, filter: int, ontology_name: str) -> List[np.ndarray]:
+
+def get_mats(
+    params: str, step: str, multi: bool, filter: int, ontology_name: str
+) -> List[np.ndarray]:
     """
     Args
     multi: bool
@@ -334,13 +252,23 @@ def get_mats(params: str, step: str, multi: bool, filter: int, ontology_name: st
     tokenizer = AutoTokenizer.from_pretrained(
         f"EleutherAI/pythia-{params}-deduped",
         revision=f"{step}",
-        cache_dir=BIGSTORAGE_DIR / "raymond" / "huggingface_cache" / f"pythia-{params}-deduped" / step
+        cache_dir=BIGSTORAGE_DIR
+        / "raymond"
+        / "huggingface_cache"
+        / f"pythia-{params}-deduped"
+        / step,
     )
 
-    g = torch.load(BIGSTORAGE_DIR / "raymond" / "pythia" / f"{params}-unembeddings" / step).to(device) # 'FILE_PATH' in store_matrices.py
+    g = torch.load(
+        BIGSTORAGE_DIR / "raymond" / "pythia" / f"{params}-unembeddings" / step
+    ).to(
+        device
+    )  # 'FILE_PATH' in store_matrices.py
 
     vocab_dict = update_vocab_dict(tokenizer)
-    cats, G, sorted_keys = hrc.get_categories_ontology(ontology_name=ontology_name, filter=filter)
+    cats, G, sorted_keys = hrc.get_categories_ontology(
+        ontology_name=ontology_name, filter=filter
+    )
 
     error_count = 0
     total_count = 0
@@ -362,12 +290,11 @@ def get_mats(params: str, step: str, multi: bool, filter: int, ontology_name: st
     adj_mat = nx.adjacency_matrix(tc_G, nodelist=sorted_keys).todense()
     adj_mat = adj_mat + adj_mat.T
 
-    lda_dirs = torch.stack([v['lda'] for k, v in dirs.items()])
-    lda_dirs = lda_dirs / lda_dirs.norm(dim = 1).unsqueeze(1)
+    lda_dirs = torch.stack([v["lda"] for k, v in dirs.items()])
+    lda_dirs = lda_dirs / lda_dirs.norm(dim=1).unsqueeze(1)
 
     for k, v in dirs.items():
         logger.info(k)
-
 
     child_parent = {}
 
@@ -377,44 +304,44 @@ def get_mats(params: str, step: str, multi: bool, filter: int, ontology_name: st
         sorted_keys.remove(node)
         logger.info(node)
 
-
     class_counter = 0
     for node in sorted_keys:
         if len(list(G.predecessors(node))) > 0:
-            parent = list(G.predecessors(node))[0]         #direct parent
+            parent = list(G.predecessors(node))[0]  # direct parent
             if parent not in list(messed_up.keys()):
-                if [*dirs[node]['lda']] == [*dirs[parent]['lda']]:
+                if [*dirs[node]["lda"]] == [*dirs[parent]["lda"]]:
                     logger.info(f"equal: {node}, {parent}")
-                child_parent.update({node:  dirs[node]['lda'] - dirs[parent]['lda']})
+                child_parent.update({node: dirs[node]["lda"] - dirs[parent]["lda"]})
         else:
             class_counter += 1
-            logger.info(f"reject: {node}")         #throws out 3
-                    
-    # lda_diff = torch.stack([lda_dirs[0]] + [v for k, v in child_parent.items()])    #adds back 1: 100 - 3 + 1 = 98
-    lda_diff = torch.stack([lda_dirs[i] for i in range(class_counter)] + [v for k, v in child_parent.items()])
+            logger.info(f"reject: {node}")  # throws out 3
 
+    # lda_diff = torch.stack([lda_dirs[0]] + [v for k, v in child_parent.items()])    #adds back 1: 100 - 3 + 1 = 98
+    lda_diff = torch.stack(
+        [lda_dirs[i] for i in range(class_counter)]
+        + [v for k, v in child_parent.items()]
+    )
 
     logger.info(f"norms: {lda_diff.norm(dim = 1)}")
     for thing in lda_diff:
         logger.info(str(thing))
 
-    lda_diff = lda_diff / lda_diff.norm(dim = 1).unsqueeze(1)
-
+    lda_diff = lda_diff / lda_diff.norm(dim=1).unsqueeze(1)
 
     # multiplying by transpose to get cosine similarity
     # num_concepts x embedding_dim * embedding_dim x num_concepts = num_concepts x num_concepts
-    mats = [adj_mat,
-            (lda_dirs @ lda_dirs.T).cpu().numpy(),
-            (lda_diff @ lda_diff.T).cpu().numpy()]
+    mats = [
+        adj_mat,
+        (lda_dirs @ lda_dirs.T).cpu().numpy(),
+        (lda_diff @ lda_diff.T).cpu().numpy(),
+    ]
 
-    return mats 
-
-
+    return mats
 
 
 if __name__ == "__main__":
     # import argparse
-    
+
     # parser = argparse.ArgumentParser(description='Generate ontology heatmaps')
     # parser.add_argument('--params', type=str, default='70M',
     #                     help='Model parameter size (default: 70M)')
@@ -427,14 +354,14 @@ if __name__ == "__main__":
     # parser.add_argument('--filter', type=int, default=15,
     #                     help='Filter out synsets with less than this many terms (default: 15)')
     # args = parser.parse_args()
-    
+
     # step = f"step{args.step_int}"
 
     # save_ontology_hypernym(args.params, step, ontology_name=args.ontology_name, multi=args.multiword)
     # mats = get_mats(
-    #     params=args.params, 
-    #     step=step, 
-    #     multi=args.multiword, 
+    #     params=args.params,
+    #     step=step,
+    #     multi=args.multiword,
     #     filter=args.filter,
     #     ontology_name=args.ontology_name
     # )
