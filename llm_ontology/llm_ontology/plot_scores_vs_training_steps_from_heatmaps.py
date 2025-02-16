557bff704dcd381166a77e54ab1951ff84fc4572
Gabriel Simmons <simmons.gabe@gmail.com>
2025-02-16 12:52:01 -0800

    move global scope code, separate load_scores function

--- llm_ontology/llm_ontology/plot_scores_vs_training_steps_from_heatmaps.py
+++ llm_ontology/llm_ontology/plot_scores_vs_training_steps_from_heatmaps.py
@@ -6,11 +6,13 @@ import torch
 import numpy as np
 import logging
 import pathlib
+from utils import savefig, figname_from_fig_metadata
 from .ontology_scores import (
     causal_sep_score_simple as causal_sep_score,
     hierarchy_score_simple as hierarchy_score,
     linear_rep_score_simple as linear_rep_score
 )
+from utils import read_olmo_model_names, sample_from_steps
 
 # Global paths
 BIGSTORAGE_DIR = pathlib.Path("/mnt/bigstorage")
@@ -24,18 +26,7 @@ if not logger.handlers:
     logger.addHandler(handler)
     logger.setLevel(logging.INFO)
 
-def save_plot(score: str, output_dir: str, model_name: str, parameter_models, steps):
-
-    if score == "causal_sep":
-        title = "Causal Separability Scores Multi Word"
-        y_title = "causal-sep-score"
-    elif score == "hierarchy":
-        title = "Hierarchy Scores Multi Word"
-        y_title = "hierarchy-score"
-    elif score == "linear":
-        title = "Linear Representation Scores Multi Word"
-        y_title = "linear-rep-score"
-
+def load_scores(parameter_models: list[str], steps: list[str], model_name: str, score: str):
     scores = []     # each element is a list of scores for a parameter model
 
     for parameter_model in parameter_models:
@@ -61,6 +52,22 @@ def save_plot(score: str, output_dir: str, model_name: str, parameter_models, st
 
         scores.append(temp_scores)
 
+    return scores
+
+def save_plot(score: str, output_dir: str, model_name: str, parameter_models, steps) -> None:
+
+    if score == "causal_sep":
+        title = "Causal Separability Scores Multi Word"
+        y_title = "causal-sep-score"
+    elif score == "hierarchy":
+        title = "Hierarchy Scores Multi Word"
+        y_title = "hierarchy-score"
+    elif score == "linear":
+        title = "Linear Representation Scores Multi Word"
+        y_title = "linear-rep-score"
+    
+    scores = load_scores(parameter_models, steps, model_name, score)
+
     new_scores = []  # each element is a list of scores for a step, formatted for df.DataFrame
     for i in range(len(steps)):
         new_scores.append([score_list[i] for score_list in scores])
@@ -85,97 +92,105 @@ def save_plot(score: str, output_dir: str, model_name: str, parameter_models, st
         "color": "Model Size:N",
         "tooltip": ["Step", "Score", "Model Size"]
     }
+    vis_ideas = [vis_idea]
 
-    line = alt.Chart(df).mark_line(interpolate="linear").encode(**vis_idea).interactive()
-    #TODO parameterize the visualization ideas
+    for vis_idea in vis_ideas:
 
-    # Transparent selectors across the chart. This is what tells us
-    # the x-value of the cursor
-    selectors = alt.Chart(df).mark_point().encode(
-        # TODO collect these args into a dict
-        x=alt.X('Step:Q', title='Steps', scale=alt.Scale(nice=False)),
-        opacity=alt.value(0),
-    ).add_params(
-        nearest
-    )
-    when_near = alt.when(nearest)
+        line = alt.Chart(df).mark_line(interpolate="linear").encode(**vis_idea).interactive()
+        #TODO parameterize the visualization ideas
 
-    # Draw points on the line, and highlight based on selection
-    points = line.mark_point().encode(
-        opacity=when_near.then(alt.value(1)).otherwise(alt.value(0))
-    )
+        # Transparent selectors across the chart. This is what tells us
+        # the x-value of the cursor
+        selectors = alt.Chart(df).mark_point().encode(
+            # TODO collect these args into a dict
+            x=alt.X('Step:Q', title='Steps', scale=alt.Scale(nice=False)),
+            opacity=alt.value(0),
+        ).add_params(
+            nearest
+        )
+        when_near = alt.when(nearest)
 
-    # Draw text labels near the points, and highlight based on selection
-    text = line.mark_text(align="left", dx=5, dy=-5).encode(
-        text=when_near.then("Score:Q").otherwise(alt.value(" "))
-    )
+        # Draw points on the line, and highlight based on selection
+        points = line.mark_point().encode(
+            opacity=when_near.then(alt.value(1)).otherwise(alt.value(0))
+        )
 
-    # Draw a rule at the location of the selection
-    rules = alt.Chart(df).mark_rule(color="gray").encode(
-        x=alt.X('Step:Q', title='Steps', scale=alt.Scale(nice=False)),
-    ).transform_filter(
-        nearest
-    )
-    
-    properties_dict = {
-        'width': 400,
-        'height': 300,
-        'title': title
-    }
+        # Draw text labels near the points, and highlight based on selection
+        text = line.mark_text(align="left", dx=5, dy=-5).encode(
+            text=when_near.then("Score:Q").otherwise(alt.value(" "))
+        )
 
-    # Put the five layers into a chart and bind the data
-    final_chart = alt.layer(
-        line, selectors, points, rules, text
-    ).properties(**properties_dict).interactive()
+        # Draw a rule at the location of the selection
+        rules = alt.Chart(df).mark_rule(color="gray").encode(
+            x=alt.X('Step:Q', title='Steps', scale=alt.Scale(nice=False)),
+        ).transform_filter(
+            nearest
+        )
+        
+        properties_dict = {
+            'width': 400,
+            'height': 300,
+            'title': title
+        }
 
-    # TODO extend savefig from https://github.com/g-simmons/persona-research-internship/issues/230 function to handle altair charts
-    # TODO get a filename from get_figname_from_fig_metadata
-    # TODO call savefig with the chart and filename
-    final_chart.save(f'{output_dir}.png')
-    final_chart.save(f'{output_dir}.html')
-    
-    return final_chart
+        # Put the five layers into a chart and bind the data
+        final_chart = alt.layer(
+            line, selectors, points, rules, text
+        ).properties(**properties_dict).interactive()
+
+        metadata = {
+            "score": score,
+            "title": title,
+            "y_title": y_title
+        }
+        figure_name = figname_from_fig_metadata(metadata)
+
+        savefig(
+            fig=final_chart,
+            figures_dir=output_dir,
+            figure_name=figure_name,
+            formats=["png", "html"],
+            data=df
+        )
+
+def main():
+    model_name = "olmo"
+    script_dir = pathlib.Path(__file__).parent
+    figures_dir = script_dir.parent / "figures"
+
+    if model_name == "pythia":
+        # stuff
+        steps = [f"step{i}" for i in range(1000, 145000, 2000)]
+        parameter_models = ["70M", "160M", "1.4B", "2.8B", "12B"]
+
+        pythia_dir = figures_dir / "model_score_plots_pythia_multi"
+        plot1 = save_plot("causal_sep", str(pythia_dir / "causal_sep_scores"), model_name, parameter_models, steps)
+        plot2 = save_plot("hierarchy", str(pythia_dir / "hierarchy_scores"), model_name, parameter_models, steps)
+        plot3 = save_plot("linear", str(pythia_dir / "linear_rep_scores"), model_name, parameter_models, steps)
+
+        combined = alt.hconcat(plot1, plot2, plot3)
+        combined.save(str(pythia_dir / "combined_scores.html"))
+        combined.save(str(pythia_dir / "combined_scores.png"))
+
+    if model_name == "olmo":
+        # stuff
+        data_path = script_dir.parent / "data" / "olmo_7B_model_names.txt"
+        steps = read_olmo_model_names()
+        logger.info(f"Number of steps: {len(steps)}")
+        newsteps = sample_from_steps(steps)
+        logger.info(f"Selected steps: {newsteps}")
+        parameter_models = ["7B"]
+
+        # saving plots
+        olmo_dir = figures_dir / "model_score_plots_olmo_multi"
+        plot1 = save_plot("causal_sep", str(olmo_dir / "causal_sep_scores"), model_name, parameter_models, newsteps)
+        plot2 = save_plot("hierarchy", str(olmo_dir / "hierarchy_scores"), model_name, parameter_models, newsteps)
+        # plot3 = save_plot("linear", str(olmo_dir / "linear_rep_scores"), model_name, parameter_models, newsteps)
+
+        combined = alt.hconcat(plot1, plot2)
+        combined.save(str(olmo_dir / "combined_scores.html"))
+        combined.save(str(olmo_dir / "combined_scores.png"))
 
 
-model_name = "olmo"
-script_dir = pathlib.Path(__file__).parent
-figures_dir = script_dir.parent / "figures"
-
-if model_name == "pythia":
-    # stuff
-    steps = [f"step{i}" for i in range(1000, 145000, 2000)]
-    parameter_models = ["70M", "160M", "1.4B", "2.8B", "12B"]
-
-    pythia_dir = figures_dir / "model_score_plots_pythia_multi"
-    plot1 = save_plot("causal_sep", str(pythia_dir / "causal_sep_scores"), model_name, parameter_models, steps)
-    plot2 = save_plot("hierarchy", str(pythia_dir / "hierarchy_scores"), model_name, parameter_models, steps)
-    plot3 = save_plot("linear", str(pythia_dir / "linear_rep_scores"), model_name, parameter_models, steps)
-
-    combined = alt.hconcat(plot1, plot2, plot3)
-    combined.save(str(pythia_dir / "combined_scores.html"))
-    combined.save(str(pythia_dir / "combined_scores.png"))
-
-if model_name == "olmo":
-    # stuff
-    data_path = script_dir.parent / "data" / "olmo_7B_model_names.txt"
-    with open(data_path, "r") as a:
-        steps = a.readlines()
-    steps = list(map(lambda x: x[:-1], steps))
-    steps.sort(key=lambda x: int(x.split("-")[0].split("p")[1]))
-    logger.info(f"Number of steps: {len(steps)}")
-    newsteps = []
-    for i in range(len(steps)):
-        if i % 15 == 0:
-            newsteps.append(steps[i])
-    logger.info(f"Selected steps: {newsteps}")
-    parameter_models = ["7B"]
-
-    # saving plots
-    olmo_dir = figures_dir / "model_score_plots_olmo_multi"
-    plot1 = save_plot("causal_sep", str(olmo_dir / "causal_sep_scores"), model_name, parameter_models, newsteps)
-    plot2 = save_plot("hierarchy", str(olmo_dir / "hierarchy_scores"), model_name, parameter_models, newsteps)
-    # plot3 = save_plot("linear", str(olmo_dir / "linear_rep_scores"), model_name, parameter_models, newsteps)
-
-    combined = alt.hconcat(plot1, plot2)
-    combined.save(str(olmo_dir / "combined_scores.html"))
-    combined.save(str(olmo_dir / "combined_scores.png"))
+if __name__ == "__main__":
+    main()
