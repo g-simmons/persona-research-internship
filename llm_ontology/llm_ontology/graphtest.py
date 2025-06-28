baef15806cab98e4ef180903faa0f1fe769ada43
chance <cjtokubo@ucdavis.edu>
2025-06-28 11:33:37 -0700

    recent push testing

--- llm_ontology/llm_ontology/graphtest.py
+++ llm_ontology/llm_ontology/graphtest.py
@@ -4,7 +4,7 @@ x = [1,2,3,4,5]
 y = [1,2,3,4,5]
 
 plt.plot(x,y)
-plt.title('Repository Secrets')
-plt.xlabel('testing122223333')
-plt.ylabel('Testing123')
+plt.title('Checking Recent')
+plt.xlabel('Checking')
+plt.ylabel('Recent')
 plt.show()
