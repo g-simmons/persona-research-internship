282094b7d8958b469329464ec5cbd85afee9989c
chance <cjtokubo@ucdavis.edu>
2025-07-01 09:37:56 -0700

    graphtesting

--- llm_ontology/llm_ontology/graphtest.py
+++ llm_ontology/llm_ontology/graphtest.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-x = [1,2,3,4,5]
-y = [1,2,3,4,5]
+x1 = np.linspace(0.0, 5.0, 100)
+y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
 
-plt.plot(x,y)
-plt.title('JSON TESTING')
-plt.xlabel('PARSE')
-plt.ylabel('TESTING')
+fig, ax = plt.subplots(figsize=(5, 3))
+fig.subplots_adjust(bottom=0.15, left=0.2)
+ax.plot(x1, y1)
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('Damped oscillation (V)')
 plt.show()
